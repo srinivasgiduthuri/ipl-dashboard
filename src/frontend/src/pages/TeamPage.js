7720a456ae201ff693e53ea488bc29208348b583
Srinivas Giduthuri <giduthurisrinivas@gmail.com>
2024-04-22 06:46:30 +0100

    Add react query

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,25 +1,34 @@
-import { React, useEffect, useState } from "react";
+import { React } from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useQuery } from "@tanstack/react-query";
+import { fetcherWithFetch } from "../lib/fetcher";
 
 export const TeamPage = () => {
-  const [team, setTeam] = useState({ matches: [] });
-  useEffect(() => {
-    const fetchMatches = async () => {
-      const response = await fetch("http://localhost:8080/team/Delhi Capitals");
-      const data = await response.json();
-      setTeam(data);
-    };
-    fetchMatches();
-  }, []);
+  const {
+    data,
+    error,
+    isPending: loading,
+  } = useQuery({
+    queryKey: ["matches"],
+    queryFn: () =>
+      fetcherWithFetch(`http://localhost:8080/team/Delhi Capitals`),
+  });
 
   return (
     <div className="TeamPage">
-      <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]} />
-      {team.matches.slice(1).map((match) => (
-        <MatchSmallCard match={match} key={match.id} />
-      ))}
+      {loading && <div>Loading...</div>}
+      {error && <div>Error</div>}
+
+      {data && (
+        <div>
+          <h1>{data.teamName}</h1>
+          <MatchDetailCard match={data.matches[0]} />
+          {data.matches.slice(1).map((match) => (
+            <MatchSmallCard match={match} key={match.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
