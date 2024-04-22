0d1d2e56782f724f344e8ce0bcd39ee407fcb989
Srinivas Giduthuri <giduthurisrinivas@gmail.com>
2024-04-22 16:39:27 +0100

    Add react routes

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,31 +1,36 @@
 import { React } from "react";
+import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { useQuery } from "@tanstack/react-query";
 import { fetcherWithFetch } from "../lib/fetcher";
 
 export const TeamPage = () => {
+  const { teamName } = useParams();
   const {
     data,
     error,
     isPending: loading,
   } = useQuery({
-    queryKey: ["matches"],
-    queryFn: () =>
-      fetcherWithFetch(`http://localhost:8080/team/Delhi Capitals`),
+    queryKey: ["matches", teamName],
+    queryFn: () => fetcherWithFetch(`http://localhost:8080/team/${teamName}`),
   });
 
   return (
     <div className="TeamPage">
       {loading && <div>Loading...</div>}
-      {error && <div>Error</div>}
+      {error && <div>Team not found.</div>}
 
       {data && (
         <div>
           <h1>{data.teamName}</h1>
-          <MatchDetailCard match={data.matches[0]} />
+          <MatchDetailCard teamName={data.teamName} match={data.matches[0]} />
           {data.matches.slice(1).map((match) => (
-            <MatchSmallCard match={match} key={match.id} />
+            <MatchSmallCard
+              teamName={data.teamName}
+              match={match}
+              key={match.id}
+            />
           ))}
         </div>
       )}
