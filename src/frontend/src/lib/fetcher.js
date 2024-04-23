b5d9d823b2a2845aeaf778bc7b7867b6ab826ae4
Srinivas Giduthuri <giduthurisrinivas@gmail.com>
2024-04-23 10:09:17 +0100

    Remove UI code and place in different project

--- src/frontend/src/lib/fetcher.js
+++
@@ -1,9 +0,0 @@
-export const fetcherWithFetch = async (url) => {
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new Error(`HTTP error: Status ${response.status}`);
-  }
-
-  return response.json();
-};
